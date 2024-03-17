20357608db3395301c3a1ef7cb37bc1c6941fd4e
kazusatouma0528 <93085901+kazusatouma0528@users.noreply.github.com>
2024-03-17 13:35:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,7 @@
  */
 const USER_CONFIG = {
 
-  USE_PASSAGE: 'push-plus',
+  USE_PASSAGE: 'server-chan',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '笨笨',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ce45355538918ce381eb8af97e424c57',
+      id: 'SCT242438Ttrs0m4IhsW8gGS4O5jBUqiR0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
