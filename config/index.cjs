c5360a2c87abd0455873333f748702090eb1f93e
kazusatouma0528 <93085901+kazusatouma0528@users.noreply.github.com>
2024-03-17 14:26:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oxKGd6MBl1FOqnyOuwA0DxnoZUtg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '0mbZtZKVW2L6nyV4S_V55_J68VPoypbDh26X9olrr_4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-10',
       festivals: [
