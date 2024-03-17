40f63039005d9ddbdcbaba8ed50f9f1949d7e2f8
kazusatouma0528 <93085901+kazusatouma0528@users.noreply.github.com>
2024-03-17 15:12:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,6 +7,8 @@
  */
 const USER_CONFIG = {
 
+  USE_PASSAGE: 'push-deer',
+  
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx90e8ce5d456179ca',
 
@@ -21,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '笨笨',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oxKGd6MBl1FOqnyOuwA0DxnoZUtg',
+      id: 'KeyyG7hVtqP',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '1rYZGy3peHMfjKzG1EBtm9U0fgfl7ZJTEinrT6zOqAc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
